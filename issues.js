2770fb5b3264aabba0819b44fcf368aca65d1f90
Sabrina Jones <sabrina.jones87@gmail.com>
2017-12-19 23:32:25 +0000

    automatically backed up by learn

--- issues.js
+++ issues.js
@@ -1,3 +1,18 @@
+<script id="issue-template" type="text/x-handlebars-template">
+  {{#each this}}
+  <article>
+    <header><h3>Issue #{{number}} ({{state}})</h3></header>
+    <p>{{body}}</p>
+    {{#if comments_count}}
+    <p>{{comments_count}} Comments</p>
+    {{else}}
+    <p>No Comments Yet</p>
+    {{/if}}
+    <footer><a href="{{url}}">created {{created_at}}</a></footer>
+  </article>
+  {{/each}}
+</script>
+
 var issues = [
 {
   "body": "Instructions say GET /team and POST /newteam. Rspec wants GET/newteam and POST/team.",
